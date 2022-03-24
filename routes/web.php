ad51f591f3a01f84ddfbb1034007facc71ce83ef
cesarin-ex116 <98924567+cesarin-ex116@users.noreply.github.com>
2022-03-24 15:21:18 -0700

    version correcion de error

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\http\controllers\NotaController;
+use App\http\Controllers\NotaController;
 
 /*
 |--------------------------------------------------------------------------
