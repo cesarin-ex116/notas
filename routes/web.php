7976cbde7861f8ea1b4645dc67b0d4939190e7f6
cesarin-ex116 <98924567+cesarin-ex116@users.noreply.github.com>
2022-03-24 15:29:27 -0700

    Se configuran los archivos para heroku

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\http\Controllers\NotaController;
+use App\Http\Controllers\NotaController;
 
 /*
 |--------------------------------------------------------------------------
